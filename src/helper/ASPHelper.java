a421f39b0fc72dad71d40fe2f02095898c1088fd
Yohanna Lisnichuk <ylisnichuk@pol.una.py>
2015-10-29 15:56:20 -0300

    Se corrige generacion de tabla ASP

--- src/helper/ASPHelper.java
+++ src/helper/ASPHelper.java
@@ -27,244 +27,319 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
 import src.helper.clases.Produccion;
 
 /**
- *
+ * 
  * @author Yohanna Lisnichuk
  * @since 1.0
  * @version 1.0 25/10/2015
- *
+ * 
  */
 public class ASPHelper {
 
-	public static final String VACIO = "vacio";
-	private List<Produccion> producciones;
-	private List<String> conjuntoPrimero;
-	private List<String> siguientesEnProceso;
+    public static final String VACIO = "vacio";
+    private List<Produccion> producciones;
+    private List<String> conjuntoPrimero;
+    private List<String> siguientesEnProceso;
 
-	public ASPHelper(List<Produccion> producciones) {
+    public ASPHelper(List<Produccion> producciones) {
 
-		super();
-		this.producciones = producciones;
-		this.conjuntoPrimero = new ArrayList<String>();
-	}
+        super();
+        this.producciones = producciones;
+        this.conjuntoPrimero = new ArrayList<String>();
+    }
 
-	public Set<String> getTerminales() {
+    public Set<String> getTerminales() {
 
-		Set<String> aRet = new HashSet<String>();
-		for (Produccion prod : getProducciones()) {
-			for (String variable : prod.getDerecha()) {
-				if (esTerminal(variable)) {
-					aRet.add(variable);
-				}
-			}
-		}
-		return aRet;
-	}
+        Set<String> aRet = new HashSet<String>();
+        for (Produccion prod : getProducciones()) {
+            for (String variable : prod.getDerecha()) {
+                if (esTerminal(variable)) {
+                    aRet.add(variable);
+                }
+            }
+        }
+        return aRet;
+    }
 
-	public Set<String> getNoTerminales() {
+    public Set<String> getNoTerminales() {
 
-		Set<String> aRet = new HashSet<String>();
-		for (Produccion prod : getProducciones()) {
-			if (!esTerminal(prod.getIzquierda())) {
-				aRet.add(prod.getIzquierda());
-			}
-		}
-		return aRet;
-	}
+        Set<String> aRet = new HashSet<String>();
+        for (Produccion prod : getProducciones()) {
+            if (!esTerminal(prod.getIzquierda())) {
+                aRet.add(prod.getIzquierda());
+            }
+        }
+        return aRet;
+    }
 
-	/**
-	 * Metodo para determinar si una variable dada es o no un terminal, segun la
-	 * lista de producciones seteada en el constructor de la clase
-	 *
-	 * @param variable
-	 * @return
-	 */
-	public boolean esTerminal(String variable) {
+    /**
+     * Metodo para determinar si una variable dada es o no un terminal, segun la
+     * lista de producciones seteada en el constructor de la clase
+     * 
+     * @param variable
+     * @return
+     */
+    public boolean esTerminal(String variable) {
 
-		for (Produccion prod : getProducciones()) {
-			if (prod.getIzquierda().contains(variable)) {
-				return false;
-			}
+        for (Produccion prod : getProducciones()) {
+            if (prod.getIzquierda().contains(variable)) {
+                return false;
+            }
 
-		}
-		return true;
+        }
+        return true;
 
-	}
+    }
 
-	/**
-	 * Metodo para obtener el conjunto primero de una variable dada
-	 *
-	 * @param variable
-	 * @return
-	 */
-	public Set<String> getPrimero(String variable) {
+    /**
+     * Metodo para obtener el conjunto primero de una variable dada
+     * 
+     * @param variable
+     * @return
+     */
+    public Set<String> getPrimero(String variable) {
 
-		// Se ponen todas las producciones como no procesadas
-		for (Produccion prod : getProducciones()) {
-			prod.setProcesado(false);
-		}
-		// Creamos un hashSet para quitar duplicados
-		Set<String> temp = new HashSet<String>();
-		List<String> aRetornar = primero(variable, new ArrayList<String>());
-		int contadorVacio = 0;
-		int contadorNoVacio = 0;
-		// Si existe la misma cantidad de variables que de vacio, significa que
-		// cada una de ellas aporto vacio, por lo tanto hay que agregar vacio al
-		// conjunto primero una vez
-		for (String term : aRetornar) {
-			if (term.equals(VACIO)) {
-				contadorVacio++;
-			} else {
-				contadorNoVacio++;
-			}
-		}
-		temp.addAll(aRetornar);
-		// Si hay diferentes cantidades de vacio y de variables, significa que
-		// no todas aportaron vacio y vacio debe eliminarse
-		if (contadorVacio != contadorNoVacio) {
+        // Se ponen todas las producciones como no procesadas
+        for (Produccion prod : getProducciones()) {
+            prod.setProcesado(false);
+        }
+        // Creamos un hashSet para quitar duplicados
+        Set<String> temp = new HashSet<String>();
+        List<String> aRetornar = primero(variable, new ArrayList<String>());
+        int contadorVacio = 0;
+        int contadorNoVacio = 0;
+        // Si existe la misma cantidad de variables que de vacio, significa que
+        // cada una de ellas aporto vacio, por lo tanto hay que agregar vacio al
+        // conjunto primero una vez
+        for (String term : aRetornar) {
+            if (term.equals(VACIO)) {
+                contadorVacio++;
+            } else {
+                contadorNoVacio++;
+            }
+        }
+        temp.addAll(aRetornar);
+        // Si hay diferentes cantidades de vacio y de variables, significa que
+        // no todas aportaron vacio y vacio debe eliminarse
+        if (contadorVacio != contadorNoVacio) {
 
-			temp.remove(VACIO);
+            temp.remove(VACIO);
 
-		}
+        }
 
-		return temp;
-	}
+        return temp;
+    }
 
-	private List<String> primero(String variable, List<String> cPrimero) {
+    public Set<String> getPrimeroProduccion(Produccion variable) {
 
-		// Si es un terminal se agrega al conjunto primero
-		if (esTerminal(variable)) {
-			cPrimero.add(variable);
+        // Se ponen todas las producciones como no procesadas
+        for (Produccion prod : getProducciones()) {
+            prod.setProcesado(false);
+        }
+        // Creamos un hashSet para quitar duplicados
+        Set<String> temp = new HashSet<String>();
+        List<String> aRetornar = primero(variable, new ArrayList<String>());
+        int contadorVacio = 0;
+        int contadorNoVacio = 0;
+        // Si existe la misma cantidad de variables que de vacio, significa que
+        // cada una de ellas aporto vacio, por lo tanto hay que agregar vacio al
+        // conjunto primero una vez
+        for (String term : aRetornar) {
+            if (term.equals(VACIO)) {
+                contadorVacio++;
+            } else {
+                contadorNoVacio++;
+            }
+        }
+        temp.addAll(aRetornar);
+        // Si hay diferentes cantidades de vacio y de variables, significa que
+        // no todas aportaron vacio y vacio debe eliminarse
+        if (contadorVacio != contadorNoVacio) {
 
-		}
+            temp.remove(VACIO);
 
-		for (Produccion prod : getProducciones()) {
-			// se pregunta si ya no se proceso para evitar duplicados
-			if (prod.getIzquierda().equals(variable) && !prod.isProcesado()) {
+        }
 
-				// la primera variable del lado derecho siempre estara en el
-				// conjunto primero
-				prod.setProcesado(true);
+        return temp;
+    }
 
-				List<String> temp = primero(prod.getDerecha().get(0), new ArrayList<String>());
-				cPrimero.addAll(temp);
-				for (int i = 0; i < prod.getDerecha().size() - 1; i++) {
-					// si la primera variable deriva en vacio, entonces hay
-					// que agregar el conjunto primero de la siguiente, y
-					// asi sucesivamente
-					if (temp.contains(VACIO)) {
-						primero(prod.getDerecha().get(i + 1), cPrimero);
+    private List<String> primero(String variable, List<String> cPrimero) {
 
-					} else {
-						break;
-					}
-				}
-			}
-		}
+        // Si es un terminal se agrega al conjunto primero
+        if (esTerminal(variable)) {
+            cPrimero.add(variable);
 
-		return cPrimero;
+        }
 
-	}
+        for (Produccion prod : getProducciones()) {
+            // se pregunta si ya no se proceso para evitar duplicados
+            if (prod.getIzquierda().equals(variable) && !prod.isProcesado()) {
 
-	public List<Produccion> getProducciones() {
+                // la primera variable del lado derecho siempre estara en el
+                // conjunto primero
+                prod.setProcesado(true);
 
-		return producciones;
-	}
+                List<String> temp = primero(prod.getDerecha().get(0),
+                        new ArrayList<String>());
+                cPrimero.addAll(temp);
+                for (int i = 0; i < prod.getDerecha().size() - 1; i++) {
+                    // si la primera variable deriva en vacio, entonces hay
+                    // que agregar el conjunto primero de la siguiente, y
+                    // asi sucesivamente
+                    if (temp.contains(VACIO)) {
+                        primero(prod.getDerecha().get(i + 1), cPrimero);
 
-	public void setProducciones(List<Produccion> producciones) {
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
 
-		this.producciones = producciones;
-	}
+        return cPrimero;
 
-	public List<String> getConjuntoPrimero() {
+    }
 
-		return conjuntoPrimero;
-	}
+    public List<String> primero(Produccion produccion, List<String> cPrimero) {
 
-	public void setConjuntoPrimero(List<String> conjuntoPrimero) {
+        // Si es un terminal se agrega al conjunto primero
+        if (esTerminal(produccion.getDerecha().get(0))) {
+            cPrimero.add(produccion.getDerecha().get(0));
 
-		this.conjuntoPrimero = conjuntoPrimero;
-	}
+        }
 
-	public Set<String> getSiguiente(Map<String, Set<String>> primeros,
-			String variable) {
+        for (Produccion prod : getProducciones()) {
+            // se pregunta si ya no se proceso para evitar duplicados
+            if (prod.equals(produccion)) {
 
-		siguientesEnProceso = new ArrayList<String>();
-		return siguiente(primeros, variable);
-	}
+                // la primera variable del lado derecho siempre estara en el
+                // conjunto primero
+                prod.setProcesado(true);
 
-	private Set<String> siguiente(Map<String, Set<String>> primeros,
-			String variable) {
+                List<String> temp = primero(prod.getDerecha().get(0),
+                        new ArrayList<String>());
+                cPrimero.addAll(temp);
+                for (int i = 0; i < prod.getDerecha().size() - 1; i++) {
+                    // si la primera variable deriva en vacio, entonces hay
+                    // que agregar el conjunto primero de la siguiente, y
+                    // asi sucesivamente
+                    if (temp.contains(VACIO)) {
+                        primero(prod.getDerecha().get(i + 1), cPrimero);
 
-		int index;
-		Set<String> siguiente = new HashSet<String>();
-		Produccion raiz = producciones.get(0);
+                    } else {
+                        break;
+                    }
+                }
+            }
+        }
 
-		siguientesEnProceso.add(variable);
+        return cPrimero;
 
-		// Se agrega "$" en siguiente(S) si S es el simbolo inicial
-		if (variable.equals(raiz.getIzquierda()))
-			siguiente.add("$");
+    }
 
-		for (Produccion prod : getProducciones()) {
-			if ((index = prod.getDerecha().indexOf(variable)) != -1) {
+    public List<Produccion> getProducciones() {
 
-				// Se agrega "$" en siguiente(S) si S es el simbolo inicial
-				if (prod.equals(raiz))
-					siguiente.add("$");
-				// Si variable es el ultimo simbolo del lado derecho del a
-				// produccion
-				// se incluye al siguiente el siguiente del lado izquierdo
-				if (prod.getDerecha().size() == (index + 1)) {
-					// Se calcula el siguiente solo si ya no fue calculado
-					if (!siguientesEnProceso.contains(prod.getIzquierda()))
-						siguiente.addAll(siguiente(primeros,
-								prod.getIzquierda()));
-				}
-				// sino se calcula el primero de lo que este a la derecha de
-				// variable
-				else {
-					int k;
-					for (k = index + 1; k <= prod.getDerecha().size() - 1; k++) {
-						Set<String> set = primeros
-								.get(prod.getDerecha().get(k));
-						// si es un terminal simplemente se agrega dicho
-						// terminal y se termina con esta produccion
-						if (set == null) {
-							siguiente.add(prod.getDerecha().get(k));
-							break;
-						}
-						// sino se agrega el primero
-						siguiente.addAll(set);
-						// Si no contiene vacio se termina con la produccion,
-						// sino se continua calculando el primero del
-						// siguiente simbolo de la produccion
-						if (!set.contains("vacio")) {
-							break;
-						}
-					}
-					// si el primero contiene vacio, se procede a agregar
-					// tambien el siguiente del lado izquierdo de la produccion
-					if (siguiente.contains("vacio")) {
-						siguiente.remove("vacio");
-						// se verifica si el segundo ya fue calculado
-						if (!siguientesEnProceso.contains(prod.getIzquierda()))
-							siguiente.addAll(siguiente(primeros,
-									prod.getIzquierda()));
-					}
-				}
+        return producciones;
+    }
 
-			}
-		}
-		siguientesEnProceso.remove(variable);
-		return siguiente;
-	}
+    public void setProducciones(List<Produccion> producciones) {
 
-	public void limpiarConjunto() {
-		setConjuntoPrimero(new ArrayList<String>());
-	}
+        this.producciones = producciones;
+    }
+
+    public List<String> getConjuntoPrimero() {
+
+        return conjuntoPrimero;
+    }
+
+    public void setConjuntoPrimero(List<String> conjuntoPrimero) {
+
+        this.conjuntoPrimero = conjuntoPrimero;
+    }
+
+    public Set<String> getSiguiente(Map<String, Set<String>> primeros,
+            String variable) {
+
+        siguientesEnProceso = new ArrayList<String>();
+        return siguiente(primeros, variable);
+    }
+
+    private Set<String> siguiente(Map<String, Set<String>> primeros,
+            String variable) {
+
+        int index;
+        Set<String> siguiente = new HashSet<String>();
+        Produccion raiz = producciones.get(0);
+
+        siguientesEnProceso.add(variable);
+
+        // Se agrega "$" en siguiente(S) si S es el simbolo inicial
+        if (variable.equals(raiz.getIzquierda())) {
+            siguiente.add("$");
+        }
+
+        for (Produccion prod : getProducciones()) {
+            if ((index = prod.getDerecha().indexOf(variable)) != -1) {
+
+                // Se agrega "$" en siguiente(S) si S es el simbolo inicial
+                // if (prod.equals(raiz)) {
+                // siguiente.add("$");
+                // }
+                // Si variable es el ultimo simbolo del lado derecho del a
+                // produccion
+                // se incluye al siguiente el siguiente del lado izquierdo
+                if (prod.getDerecha().size() == (index + 1)) {
+                    // Se calcula el siguiente solo si ya no fue calculado
+                    if (!siguientesEnProceso.contains(prod.getIzquierda())) {
+                        siguiente.addAll(siguiente(primeros,
+                                prod.getIzquierda()));
+                    }
+                }
+                // sino se calcula el primero de lo que este a la derecha de
+                // variable
+                else {
+                    int k;
+                    for (k = index + 1; k <= prod.getDerecha().size() - 1; k++) {
+                        Set<String> set = primeros
+                                .get(prod.getDerecha().get(k));
+                        // si es un terminal simplemente se agrega dicho
+                        // terminal y se termina con esta produccion
+                        if (set == null) {
+                            siguiente.add(prod.getDerecha().get(k));
+                            break;
+                        }
+                        // sino se agrega el primero
+                        siguiente.addAll(set);
+                        // Si no contiene vacio se termina con la produccion,
+                        // sino se continua calculando el primero del
+                        // siguiente simbolo de la produccion
+                        if (!set.contains("vacio")) {
+                            break;
+                        }
+                    }
+                    // si el primero contiene vacio, se procede a agregar
+                    // tambien el siguiente del lado izquierdo de la produccion
+                    if (siguiente.contains("vacio")) {
+                        siguiente.remove("vacio");
+                        // se verifica si el segundo ya fue calculado
+                        if (!siguientesEnProceso.contains(prod.getIzquierda())) {
+                            siguiente.addAll(siguiente(primeros,
+                                    prod.getIzquierda()));
+                        }
+                    }
+                }
+
+            }
+        }
+        siguientesEnProceso.remove(variable);
+        return siguiente;
+    }
+
+    public void limpiarConjunto() {
+
+        setConjuntoPrimero(new ArrayList<String>());
+    }
 
 }
--- src/helper/ASPTable.java
+++ src/helper/ASPTable.java
@@ -27,139 +27,150 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-
 import src.helper.clases.Key;
 import src.helper.clases.Produccion;
 
 /**
- *
+ * 
  * @author Yohanna Lisnichuk
  * @since 1.0
  * @version 1.0 25/10/2015
- *
+ * 
  */
 public class ASPTable {
 
-	private Map<Key, List<Produccion>> tablaASP;
-	private Map<String, Set<String>> conjuntoPrimero;
-	private Map<String, Set<String>> conjuntoSiguiente;
-	private List<Produccion> producciones;
+    private Map<Key, List<Produccion>> tablaASP;
+    private Map<String, Set<String>> conjuntoPrimero;
+    private Map<String, Set<String>> conjuntoSiguiente;
+    private List<Produccion> producciones;
 
-	public ASPTable(Map<String, Set<String>> conjuntoPrimero,
-			Map<String, Set<String>> conjuntoSiguiente,
-			List<Produccion> producciones) {
-		super();
-		this.conjuntoPrimero = conjuntoPrimero;
-		this.conjuntoSiguiente = conjuntoSiguiente;
-		this.producciones = producciones;
-		// Debe ser una lista de producciones ya que si la gramatica es ambigua
-		// puede llegar a tener varias producciones para
-		// un mismo par terminal-noTerminal
-		this.tablaASP = new HashMap<Key, List<Produccion>>();
-	}
+    public ASPTable(Map<String, Set<String>> conjuntoPrimero,
+            Map<String, Set<String>> conjuntoSiguiente,
+            List<Produccion> producciones) {
 
-	public Map<Key, List<Produccion>> generarTablaASP() {
-		ASPHelper helper = new ASPHelper(getProducciones());
-		boolean tieneVacio = true;
+        super();
+        this.conjuntoPrimero = conjuntoPrimero;
+        this.conjuntoSiguiente = conjuntoSiguiente;
+        this.producciones = producciones;
+        // Debe ser una lista de producciones ya que si la gramatica es ambigua
+        // puede llegar a tener varias producciones para
+        // un mismo par terminal-noTerminal
+        this.tablaASP = new HashMap<Key, List<Produccion>>();
+    }
 
-		for (Produccion prod : getProducciones()) {
+    public Map<Key, List<Produccion>> generarTablaASP() {
 
-			// Para cada terminal en PRIMERO(A) agregar A->alfa a M[A,a]
-			for (String variable : getConjuntoPrimero()
-					.get(prod.getIzquierda())) {
+        ASPHelper helper = new ASPHelper(getProducciones());
+        boolean tieneVacio = true;
 
-				if (helper.esTerminal(variable)
-						&& !variable.equals(ASPHelper.VACIO)) {
-					// Se controla que si ya tenia un valor para el actual par
-					// terminal/noTerminal, entonces se agrega uno mas a la
-					// lista, de lo contrario se crea una lista nueva y se
-					// agrega la produccion actual
-					Key key = new Key(prod.getIzquierda(), variable);
-					if (getTablaASP().get(key) == null) {
-						List<Produccion> prodToAdd = new ArrayList<Produccion>();
-						prodToAdd.add(prod);
-						getTablaASP().put(key, prodToAdd);
-					} else {
-						getTablaASP().get(key).add(prod);
-					}
-				}
-			}
+        for (Produccion prod : getProducciones()) {
 
-			// Se controla que el primero de alfa tenga vacio, es decir
-			// todos los primeros del lado derecho de A tengan primero
-			tieneVacio = true;
-			for (String a : prod.getDerecha()) {
-				if (!helper.esTerminal(a)
-						&& !getConjuntoPrimero().get(a).contains(
-								ASPHelper.VACIO)) {
-					tieneVacio = false;
-					break;
-				}
-				if (a.equals(ASPHelper.VACIO)) {
-					tieneVacio = true;
-					break;
-				}
-				if (helper.esTerminal(a)) {
-					tieneVacio = false;
-					break;
-				}
-				
-			}
-			// Si tiene vacio entonces para cada terminal de SIGUIENTE(A)
-			// agregar A->alfa a M[A,a]
-			if (tieneVacio) {
-				for (String siguiente : getConjuntoSiguiente().get(
-						prod.getIzquierda())) {
-					if (helper.esTerminal(siguiente)
-							&& !siguiente.equals(ASPHelper.VACIO)) {
-						Key key = new Key(prod.getIzquierda(), siguiente);
-						if (getTablaASP().get(key) == null) {
-							List<Produccion> prodToAdd = new ArrayList<Produccion>();
-							prodToAdd.add(prod);
-							getTablaASP().put(key, prodToAdd);
-						} else {
-							getTablaASP().get(key).add(prod);
-						}
-					}
-				}
-			}
+            // Para cada terminal en PRIMERO(alfa) agregar A->alfa a M[A,a]
+            Set<String> primero = helper.getPrimeroProduccion(prod);
 
-		}
+            for (String variable : primero) {
 
-		return getTablaASP();
+                if (helper.esTerminal(variable)
+                        && !variable.equals(ASPHelper.VACIO)) {
+                    // Se controla que si ya tenia un valor para el actual
+                    // par
+                    // terminal/noTerminal, entonces se agrega uno mas a la
+                    // lista, de lo contrario se crea una lista nueva y se
+                    // agrega la produccion actual
+                    Key key = new Key(prod.getIzquierda(), variable);
+                    if (getTablaASP().get(key) == null) {
+                        List<Produccion> prodToAdd = new ArrayList<Produccion>();
+                        prodToAdd.add(prod);
+                        getTablaASP().put(key, prodToAdd);
+                    } else {
+                        getTablaASP().get(key).add(prod);
+                    }
+                }
+            }
 
-	}
+            // Se controla que el primero de alfa tenga vacio, es decir
+            // todos los primeros del lado derecho de A tengan primero
+            tieneVacio = true;
+            for (String a : prod.getDerecha()) {
+                if (!helper.esTerminal(a)
+                        && !getConjuntoPrimero().get(a).contains(
+                                ASPHelper.VACIO)) {
+                    tieneVacio = false;
+                    break;
+                }
+                if (a.equals(ASPHelper.VACIO)) {
+                    tieneVacio = true;
+                    break;
+                }
+                if (helper.esTerminal(a)) {
+                    tieneVacio = false;
+                    break;
+                }
 
-	public Map<Key, List<Produccion>> getTablaASP() {
-		return tablaASP;
-	}
+            }
+            // Si tiene vacio entonces para cada terminal de SIGUIENTE(A)
+            // agregar A->alfa a M[A,a]
+            if (tieneVacio) {
+                for (String siguiente : getConjuntoSiguiente().get(
+                        prod.getIzquierda())) {
+                    if (helper.esTerminal(siguiente)
+                            && !siguiente.equals(ASPHelper.VACIO)) {
+                        Key key = new Key(prod.getIzquierda(), siguiente);
+                        if (getTablaASP().get(key) == null) {
+                            List<Produccion> prodToAdd = new ArrayList<Produccion>();
+                            prodToAdd.add(prod);
+                            getTablaASP().put(key, prodToAdd);
+                        } else {
+                            getTablaASP().get(key).add(prod);
+                        }
+                    }
+                }
+            }
 
-	public void setTablaASP(Map<Key, List<Produccion>> tablaASP) {
-		this.tablaASP = tablaASP;
-	}
+        }
 
-	public Map<String, Set<String>> getConjuntoPrimero() {
-		return conjuntoPrimero;
-	}
+        return getTablaASP();
 
-	public void setConjuntoPrimero(Map<String, Set<String>> conjuntoPrimero) {
-		this.conjuntoPrimero = conjuntoPrimero;
-	}
+    }
 
-	public Map<String, Set<String>> getConjuntoSiguiente() {
-		return conjuntoSiguiente;
-	}
+    public Map<Key, List<Produccion>> getTablaASP() {
 
-	public void setConjuntoSiguiente(Map<String, Set<String>> conjuntoSiguiente) {
-		this.conjuntoSiguiente = conjuntoSiguiente;
-	}
+        return tablaASP;
+    }
 
-	public List<Produccion> getProducciones() {
-		return producciones;
-	}
+    public void setTablaASP(Map<Key, List<Produccion>> tablaASP) {
 
-	public void setProducciones(List<Produccion> producciones) {
-		this.producciones = producciones;
-	}
+        this.tablaASP = tablaASP;
+    }
+
+    public Map<String, Set<String>> getConjuntoPrimero() {
+
+        return conjuntoPrimero;
+    }
+
+    public void setConjuntoPrimero(Map<String, Set<String>> conjuntoPrimero) {
+
+        this.conjuntoPrimero = conjuntoPrimero;
+    }
+
+    public Map<String, Set<String>> getConjuntoSiguiente() {
+
+        return conjuntoSiguiente;
+    }
+
+    public void setConjuntoSiguiente(Map<String, Set<String>> conjuntoSiguiente) {
+
+        this.conjuntoSiguiente = conjuntoSiguiente;
+    }
+
+    public List<Produccion> getProducciones() {
+
+        return producciones;
+    }
+
+    public void setProducciones(List<Produccion> producciones) {
+
+        this.producciones = producciones;
+    }
 
 }
